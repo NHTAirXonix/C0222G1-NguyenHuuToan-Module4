fb6b1053c13deb35978a1123dc178958d42020cf
NHTAirXonix <toan02041999@gmail.com>
2022-06-24 04:05:25 +0700

    update all

--- _07_Spring_data_repository/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/repository/IClassroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.ss7_c0222g1.repository;
-
-import com.codegym.ss7_c0222g1.model.Classroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClassroomRepository extends JpaRepository<Classroom, Integer> {
-}
--- _07_Spring_data_repository/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/service/IClassroomService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.ss7_c0222g1.service;
-
-import com.codegym.ss7_c0222g1.model.Classroom;
-
-import java.util.List;
-
-public interface IClassroomService {
-    List<Classroom> findAll();
-}
