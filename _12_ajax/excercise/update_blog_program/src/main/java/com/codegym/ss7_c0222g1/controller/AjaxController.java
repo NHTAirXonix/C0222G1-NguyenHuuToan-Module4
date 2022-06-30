d58387680167a4a550af504e0e47d557a2597c41
NHTAirXonix <toan02041999@gmail.com>
2022-06-30 22:22:21 +0700

    update

--- _12_ajax/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/controller/AjaxController.java
+++ _12_ajax/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/controller/AjaxController.java
@@ -13,12 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/blog/main/json")
-public class NewBlogController {
+@RequestMapping("/ajax")
+public class AjaxController {
 
     @Autowired
     private IBlogService iBlogService;
@@ -37,28 +38,4 @@ public class NewBlogController {
         }
         return new ResponseEntity<>(blogList, HttpStatus.OK);
     }
-
-    @GetMapping("/listCategory")
-    public ResponseEntity<?> showCategory(@RequestParam(name = "page", defaultValue = "0") int page) {
-        Sort sort = Sort.by("create_day").ascending().and(Sort.by("blog_title"));
-        List<Category> list = iCategoryService.findAll();
-        if (list.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(list, HttpStatus.OK);
-    }
-
-    @GetMapping("/views/{id}")
-    public ResponseEntity<?> view(@PathVariable Integer id) {
-        Blog blog = iBlogService.findById(id);
-        if (blog==null){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(blog, HttpStatus.OK);
-    }
-
-//    @PostMapping
-//    public ResponseEntity<?> saveBlog(@RequestBody Blog blog) {
-//        return new ResponseEntity<>(iBlogService.save(blog), HttpStatus.CREATED);
-//    }
 }
--- _12_ajax/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/controller/NewBlogController.java
+++ _12_ajax/excercise/update_blog_program/src/main/java/com/codegym/ss7_c0222g1/controller/NewBlogController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/blog/main/json")
+@RequestMapping("/json")
 public class NewBlogController {
 
     @Autowired
